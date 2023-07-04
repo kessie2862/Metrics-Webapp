b713c4a8b6a79ffa4d7799e535723debf8e92331
kessie <kayp3477@gmail.com>
2023-07-04 18:17:47 +0000

    Add classNames for styling and imported Footer component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { RiArrowRightSLine } from 'react-icons/ri';
 import { fetchCategories, selectCategory } from '../redux/categoriesSlice';
 import Filter from './Filter';
+import Footer from './Footer';
+import './styles/HomePage.css';
 
 function HomePage() {
   const dispatch = useDispatch();
-  const categories = useSelector((state) => state.categories.filteredCategories);
+  const categories = useSelector(
+    (state) => state.categories.filteredCategories,
+  );
   const status = useSelector((state) => state.categories.status);
   const error = useSelector((state) => state.categories.error);
 
@@ -28,24 +33,26 @@ function HomePage() {
   }
 
   return (
-    <div>
-      <h1>This is the HomePage</h1>
+    <div className="homepage-container">
       <Filter />
-      <ul>
+      <ul className="card-container">
         {categories.map((category) => (
-          <li key={category.id}>
+          <div className="card" key={category.id}>
             <Link
               to={`/details/${category.id}`}
               onClick={() => dispatch(selectCategory(category))}
             >
-              {category.name}
-              {' '}
-              -
-              {category.metricValue}
+              <div className="card-header">
+                <img src={category.icon} alt={category.name} />
+                <RiArrowRightSLine className="arrow-icon" />
+              </div>
+              <h4 className="category-text">{category.name}</h4>
+              <h4 className="category-text">{category.symbol}</h4>
             </Link>
-          </li>
+          </div>
         ))}
       </ul>
+      <Footer />
     </div>
   );
 }
