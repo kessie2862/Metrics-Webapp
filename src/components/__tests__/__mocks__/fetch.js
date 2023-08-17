f2af0b46f6a7b9995c9ed97c34d6661e065fcff5
PROSPER KESSIE <97234029+kessie2862@users.noreply.github.com>
2023-08-17 11:38:35 +0000

    Delete src directory

--- src/components/__tests__/__mocks__/fetch.js
+++
@@ -1,9 +0,0 @@
-const mockFetch = jest.fn(() => Promise.resolve({
-  json: () => Promise.resolve({ coins: [] }),
-}));
-
-export default mockFetch;
-
-test('Some random dummy test!!', () => {
-  expect(true).toBe(true);
-});
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import categoriesReducer from './categoriesSlice';
-
-const rootReducer = combineReducers({
-  categories: categoriesReducer,
-});
-
-export default rootReducer;
